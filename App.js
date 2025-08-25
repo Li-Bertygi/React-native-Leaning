161c6690bf12889243123e4b8dd0b4e77a62733c
Li-Bertygi <tokhwoo2000@gmail.com>
2025-08-25 21:11:37 +0900

    Practice:studied how to implement an API to display news articles

--- App.js
+++ App.js
@@ -1,53 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import NewsKizi from "./components/NewsKizi";
+import Constants from "expo-constants";
+import axios from "axios";
+
+const URI = `https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=${Constants.expoConfig.extra.newsApiKey}`;
 
 export default function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getNews = async () => {
+    const response = await axios.get(URI);
+    setNews(response.data.articles);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <View style={styles.moziBox}>
-          <Text style={styles.Text}>犬です。</Text>
-          <Text style={styles.subText}>黒い</Text>
-        </View>
-        <View style={styles.gazoBox}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{
-              uri: "https://picsum.photos/id/237/200/300",
-            }}
+      <FlatList
+        data={news}
+        renderItem={({ item }) => (
+          <NewsKizi
+            imageuri={item.urlToImage}
+            title={item.title}
+            subtext={item.publishedAt}
           />
-        </View>
-      </View>
-
-      <View style={styles.box}>
-        <View style={styles.moziBox}>
-          <Text style={styles.Text}>テスト。</Text>
-          <Text style={styles.subText}>サブテスト</Text>
-        </View>
-        <View style={styles.gazoBox}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{
-              uri: "https://picsum.photos/id/230/200/300",
-            }}
-          />
-        </View>
-      </View>
-
-      <View style={styles.box}>
-        <View style={styles.moziBox}>
-          <Text style={styles.Text}>test</Text>
-          <Text style={styles.subText}>subtest</Text>
-        </View>
-        <View style={styles.gazoBox}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{
-              uri: "https://picsum.photos/id/210/200/300",
-            }}
-          />
-        </View>
-      </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -56,36 +38,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff", // 배경색
-    alignItems: "flex-start", // 가로 배치
-    justifyContent: "space-between", // 세로 배치
-  },
-
-  box: {
-    height: 100,
-    width: "100%",
-    borderColor: "lightblue",
-    borderWidth: 1,
-    flexDirection: "row",
-  },
-
-  moziBox: {
-    flex: 1,
-    backgroundColor: "steelblue",
-    padding: 16,
-    justifyContent: "space-between",
-  },
-
-  gazoBox: {
-    width: 100,
-    backgroundColor: "powderblue",
-  },
-
-  Text: {
-    fontsize: 16,
-  },
-
-  subText: {
-    fontSize: 12,
-    color: "red",
   },
 });
